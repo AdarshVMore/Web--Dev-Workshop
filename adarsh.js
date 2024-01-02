0d43c80353e2055e9013db9cc6210e79b37cad8c
AdarshVMore <skullcrushermore@gmail.com>
2024-01-02 17:33:12 +0530

    Javascript Fundamentals done

---
+++ adarsh.js
@@ -0,0 +1,168 @@
+// console.log("hello world!");
+
+// let, var, const
+
+// let a = 100;
+// a = "adarsh";
+
+// console.log(a);
+// let a = 100;
+// a = 2000;
+// console.log(a);
+
+// let x = 100;
+// x = 500;
+
+// console.log(x);
+
+// var b = 100;
+
+// function numberChange() {
+//   var b = 99999;
+//   console.log(b);
+// }
+
+// numberChange();
+
+// console.log(b);
+
+// data Types
+// String = "100"
+// Number = 554465416532865
+// Bool = true , false
+
+let a = "156";
+
+// if (a === 156) {
+//   console.log("this is true");
+// } else {
+//   a = 156;
+//   if (a === 156) {
+//     console.log("this is nested true");
+//   }
+// }
+
+// a === 156 ?  = false
+
+// if (true) {
+//   console.log(1);
+// } else if (true) {
+//   console.log(5);
+// }
+
+// let adarsh = "boy";
+// let age = 20;
+
+// email = "mail.com";
+// password = "1111111";
+
+// if (email === "adarshv.more18@gmail.com") {
+//   if (password === "12345678") {
+//     console.log("Welcome Adarsh");
+//   } else {
+//     console.log("wrong Password");
+//   }
+// } else {
+//   console.log("bhai tera account nahi hai yaha ,u  need to register first");
+// }
+
+// + , - , ++ , -- , < , < , <= , >= , ===
+
+// for (let i = 0; i <= 10000; i+100) {
+//   i = i + 1;
+//   console.log(i);
+// }
+
+// 0 <= 10000
+// 1 <= 10000
+// 2 <= 10000
+// 2 + 1 = 3
+// 10000 + 1 = 10001 <= 10000
+
+// Question : 1
+// let firstName = "Adarsh";
+// let lastName = "More";
+
+// console.log("Good Morning " + firstName + " " + lastName);
+
+// Question : 2
+// let firstName = "XYZ";
+// let lastName = "ABC";
+// let gender = "boy";
+
+// if (gender === "boy") {
+//   console.log("Good Morning Mr " + firstName + " " + lastName);
+// } else if (gender === "girl") {
+//   console.log("Good Morning Ms " + firstName + " " + lastName);
+// }
+
+// Question : 3
+// for (let i = 0; i <= 10000; i+100) {
+//   i = i + 1;
+//   console.log(i);
+// }
+
+let x = [10, 20, 25, 30, 35];
+let z = ["hii", "hello", "GM", "GN"];
+
+let y = {
+  name: "adarsh",
+  age: 25,
+  college: "GIT",
+  KT: true,
+};
+
+console.log(x[3] + 1000);
+
+console.log(y.KT);
+
+let User = {
+  email: "adarsh@gmail.com",
+  password: "11111111",
+};
+
+let userData = [
+  {
+    email: "adarsh@gmail.com",
+    password: "11111111",
+  },
+  {
+    email: "adah@gmail.com",
+    password: "111adfs1111",
+  },
+  {
+    email: "ash@gmail.com",
+    password: "11awesdfeads111111",
+  },
+  {
+    email: "adarsh@645gmail.com",
+    password: "111afdcads111",
+  },
+  {
+    email: "adsh@gmail.m",
+    password: "1111111asdfcad1",
+  },
+  {
+    email: "qqqqq@gmail.com",
+    password: "qqqqqqq",
+  },
+];
+
+let email = "qqqq@gmail.com";
+let password = "qqqqqq";
+
+for (let i = 0; i < userData.length; i++) {
+  if (email === userData[i].email) {
+    if (password === userData[i].password) {
+      console.log("Welcome " + userData[i].email);
+    } else {
+      console.log("wrong Password");
+    }
+    // console.log("ahe bhai email tuza");
+  } else {
+    console.log("bhai tera account nahi hai yaha ,u  need to register first");
+  }
+  //   console.log(userData[i].email);
+}
+
+// userData[i];
